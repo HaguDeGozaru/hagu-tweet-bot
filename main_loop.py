b545bec6a7b7e65aa703f8cebc411cb9af1e1943
Ian M <hagudegozaru@gmail.com>
2017-05-10 17:30:11 +0900

    Successful API test.

--- main_loop.py
+++ main_loop.py
@@ -1,12 +1,15 @@
 import tweepy
+from credentials import *
 from time import sleep
 
-auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret)
-auth.set_access_token(credentials.access_token, credentials.access_token_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-'my_file=open('verne.txt','r')
-'file_lines=my_file.readlines()
-'my_file.close()
+'''
+my_file=open('verne.txt','r')
+file_lines=my_file.readlines()
+my_file.close()
+'''
 
 api.update_status("Hello world!")
